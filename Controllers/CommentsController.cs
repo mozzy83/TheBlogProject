8ab0906a05e026e50bddb5a47da1b29689187e35
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-02-12 18:13:23 -0700

    Initial Comments scaffolding edited

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -20,41 +20,40 @@ namespace TheBlogProject.Controllers
         }
 
         // GET: Comments
+
+        //public async Task<IActionResult> OriginalIndex()
+        //{
+        //    var originalComments = await _context.Comments.ToListAsync();
+        //    return View("Index", originalComments);
+        //}
+
+        //public async Task<IActionResult> ModeratedIndex()
+        //{
+        //    var moderatedComments = await _context.Comments.Where(c => c.Moderated != null).ToListAsync();
+        //    return View("Index", moderatedComments);
+
+
+        //}
+
+        //public async Task<IActionResult> DeletedIndex() 
+        //{
+        //Use soft delet bool
+        //}
+
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Comments.Include(c => c.BlogUser).Include(c => c.Moderator).Include(c => c.Post);
-            return View(await applicationDbContext.ToListAsync());
-        }
-
-        // GET: Comments/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Comments == null)
-            {
-                return NotFound();
-            }
-
-            var comment = await _context.Comments
-                .Include(c => c.BlogUser)
-                .Include(c => c.Moderator)
-                .Include(c => c.Post)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-
-            return View(comment);
+            var allComments = await _context.Comments.ToListAsync();
+            return View("Index", allComments);
         }
 
         // GET: Comments/Create
-        public IActionResult Create()
-        {
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract");
-            return View();
-        }
+        //public IActionResult Create()
+        //{
+        //    ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id");
+        //    ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id");
+        //    ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract");
+        //    return View();
+        //}
 
         // POST: Comments/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -69,9 +68,7 @@ namespace TheBlogProject.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", comment.BlogUserId);
-            ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id", comment.ModeratorId);
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", comment.PostId);
+
             return View(comment);
         }
 
@@ -167,14 +164,14 @@ namespace TheBlogProject.Controllers
             {
                 _context.Comments.Remove(comment);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool CommentExists(int id)
         {
-          return _context.Comments.Any(e => e.Id == id);
+            return _context.Comments.Any(e => e.Id == id);
         }
     }
 }
