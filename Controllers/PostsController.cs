f6274b03fd0c83422f49ec07d0f73f6f8eab7b95
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-02-19 18:17:12 -0700

    Images in Blogs and Posts
    Updated Edits to not overwrite current values if nothing new entered. Updated views to use image service

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -15,11 +15,13 @@ namespace TheBlogProject.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly ISlugService _slugService;
+        private readonly IImageService _imageService;
 
-        public PostsController(ApplicationDbContext context, ISlugService slugService)
+        public PostsController(ApplicationDbContext context, ISlugService slugService, IImageService imageService)
         {
             _context = context;
             _slugService = slugService;
+            _imageService = imageService;
         }
 
         // GET: Posts
@@ -68,6 +70,12 @@ namespace TheBlogProject.Controllers
             {
                 post.Created = DateTime.UtcNow;
 
+                //Use _imageService to store the incoming user specified image
+                post.ImageData = await _imageService.EncodeImageAsync(post.Image);
+                post.ContentType = _imageService.ContentType(post.Image);
+
+
+
                 //Create the slug and determine if it is unique
                 var slug = _slugService.UrlFriendly(post.Title);
                 if (!_slugService.IsUnique(slug))
@@ -111,7 +119,7 @@ namespace TheBlogProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus,Image")] Post post)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus")] Post post, IFormFile? newImage)
         {
             if (id != post.Id)
             {
@@ -122,9 +130,21 @@ namespace TheBlogProject.Controllers
             {
                 try
                 {
-                    post.Updated = DateTime.UtcNow;
+                    var newPost = await _context.Posts.FindAsync(post.Id);
                     
-                    _context.Update(post);
+                    newPost.Updated = DateTime.UtcNow;
+                    newPost.Title = post.Title;
+                    newPost.Abstract = post.Abstract;
+                    newPost.Content = post.Content;
+                    newPost.ReadyStatus = post.ReadyStatus;
+
+                    if(newImage is not null)
+                    {
+                        newPost.ImageData = await _imageService.EncodeImageAsync(newImage);
+                        newPost.ContentType = _imageService.ContentType(newImage);
+                    }
+                    
+                    
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
