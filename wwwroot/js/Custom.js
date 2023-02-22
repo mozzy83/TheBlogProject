caf4191d0327dd31348965945139691bcf6f4492
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2023-02-21 21:20:05 -0700

    Edit Tags/Sweetalerts

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -3,9 +3,20 @@
 function AddTag() {
     var tagEntry = document.getElementById("TagEntry");
 
-    //create a new select option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-    document.getElementById("TagList").options[index++] = newOption;
+    //Use search function to help detect an error state
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        //Trigger sweetalert for condition contained in searchResult var
+        swalWithDarkButton.fire({
+            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span>`
+        });
+    }
+    else {
+        //create a new select option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+    }
+
 
     //clear TagEntry control
     tagEntry.value = "";
@@ -14,12 +25,18 @@ function AddTag() {
 
 function DeleteTag() {
     let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: "<span class='font-weight-bolder'>CHOOSE A TAG BEFORE DELETING</span>"
+        });
+        return true;
+    }
 
     while (tagCount > 0) {
-        let tagList = document.getElementById("TagList");
-        let selectedIndex = tagList.selectedIndex;
-        if (selectedIndex >= 0) {
-            tagList.options[selectedIndex] = null;
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         }
         else
@@ -46,3 +63,30 @@ function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag)
     document.getElementById("TagList").options[index] = newOption;
 }
+
+//The Search function will detect either an empty or a duplicate tag
+//and return an error string if error is detected
+function search(str) {
+    if (str == "") {
+        return "Empty tags are not permitted";
+    }
+
+    var tagsEl = document.getElementById('TagList');
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let index = 0; index < options.length; index++) {
+            if (options[index].value == str)
+                return `The Tag #${str} was detected as a duplicate and is not permitted`
+        }
+    }
+
+}
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger btn-sm btn-block btn-outline-dark'
+    },
+    imageUrl: '/images/Oops.jpg',
+    timer: 3250,
+    buttonsStyling: false
+})
